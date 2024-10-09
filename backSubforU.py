f4424bcec004e4b6cf50734837a791519cb7b4d9
Hemil <hemilpats@gmail.com>
2024-10-08 20:52:48 -0700

    backSubAlgo

--- backSubforU.py
+++ backSubforU.py
@@ -1,4 +1,14 @@
-import numpy
+import numpy as np
 
-def backSubstitution(maindiag, upperdiag, b):
+def backSubstitution(upperdiag, b, maindiag):
     
+    m = len(upperdiag)
+    x = np.zeros(m)
+
+    x[m - 1] = b[m - 1] / maindiag[m - 1]
+
+    for i in range(m-2, -1, -1):
+
+        x[m - 1] = (b[m - 1] - upperdiag[i]*x[i + 1]) / maindiag[i]
+
+    return x
